072b229a89fe01c711528b4d2f644cedbcef7cb0
Alex Belan <alexbelan1999@gmail.com>
2020-08-04 16:41:52 +0300

    Fix client

--- client.py
+++ client.py
@@ -3,32 +3,26 @@ import grpc
 import datahash_pb2
 import datahash_pb2_grpc
 
-channel = grpc.insecure_channel('localhost:64000')
-stub = datahash_pb2_grpc.DataHashStub(channel)
 
-person = input('Input your name: ')
-text = input('Input message: ')
+def simle_rcp(stub, text):
+    to_hello = datahash_pb2.Text(data=text)
+    response = stub.hello(to_hello)
+    print(response.data)
 
-print("------------------------Simple RPC---------------")
-to_hello = datahash_pb2.Text(data=text)
-response = stub.hello(to_hello)
-print(response.data)
+    to_md5 = datahash_pb2.Text(data=text)
+    response = stub.hash_md5(to_md5)
+    print(response.data)
 
-to_md5 = datahash_pb2.Text(data=text)
-response = stub.hash_md5(to_md5)
-print(response.data)
+    to_sha256 = datahash_pb2.Text(data=text)
+    response = stub.hash_sha256(to_sha256)
+    print(response.data)
 
-to_sha256 = datahash_pb2.Text(data=text)
-response = stub.hash_sha256(to_sha256)
-print(response.data)
 
-print("------------------------Response-streaming RPC---------------")
-to_list = datahash_pb2.Text(data=text)
-items = stub.stream_text(to_list)
-for item in items:
-    print(item.data)
-
-print("------------------------Request-streaming RPC---------------")
+def resp_stream_rcp(stub, text):
+    to_list = datahash_pb2.Text(data=text)
+    items = stub.stream_text(to_list)
+    for item in items:
+        print(item.data)
 
 
 def generator(info: list):
@@ -36,20 +30,41 @@ def generator(info: list):
         yield i
 
 
-name = datahash_pb2.Text(data=person)
-mesg = datahash_pb2.Text(data=text)
-result_list = [name, mesg]
+def req_stream_rcp(stub, person, text):
+    name = datahash_pb2.Text(data=person)
+    mesg = datahash_pb2.Text(data=text)
+    result_list = [name, mesg]
 
-text_iterator = generator(result_list)
-response = stub.input_stream(text_iterator)
-print(response.data)
+    text_iterator = generator(result_list)
+    response = stub.input_stream(text_iterator)
+    print(response.data)
 
-print("------------------------Bidirectional streaming RPC---------------")
-name = datahash_pb2.Text(data=person)
-mesg = datahash_pb2.Text(data=text)
-result_list = [name, mesg]
 
-text_iterator = generator(result_list)
-response = stub.dual_stream(text_iterator)
-for resp in response:
-    print(resp.data)
+def bidirect_stream_rcp(stub, person, text):
+    name = datahash_pb2.Text(data=person)
+    mesg = datahash_pb2.Text(data=text)
+    result_list = [name, mesg]
+
+    text_iterator = generator(result_list)
+    response = stub.dual_stream(text_iterator)
+    for resp in response:
+        print(resp.data)
+
+
+def client():
+    with grpc.insecure_channel('localhost:64000') as channel:
+        stub = datahash_pb2_grpc.DataHashStub(channel)
+        person = input('Input your name: ')
+        text = input('Input message: ')
+        print("------------------------Simple RPC---------------")
+        simle_rcp(stub, text)
+        print("------------------------Response-streaming RPC---------------")
+        resp_stream_rcp(stub, text)
+        print("------------------------Request-streaming RPC---------------")
+        req_stream_rcp(stub, person, text)
+        print("------------------------Bidirectional streaming RPC---------------")
+        bidirect_stream_rcp(stub, person, text)
+
+
+if __name__ == '__main__':
+    client()
